51d8b9eca3012aa6461e73ef253cf3e53e34ea03
rninji <yellowminji@naver.com>
2024-03-24 00:59:00 +0900

    [#17] FIX : 글 검색 쿼리 및 로직 수정

--- src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
+++ src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
@@ -52,58 +52,25 @@ public class SearchServiceImpl implements SearchService{
      **/
     @Override
     public List<ArticleResponseDTO> articlesSearch(Long userSeq, String inputText, Long lastArticleId) {
-        Pageable pageable = PageRequest.of(0, 4);
-        List<ArticleResponseDTO> searchArticleList;
-        Page<Article> articlesByArticleText;
+        Pageable pageable = PageRequest.of(0, 4); // 한 번에 글 4개 조회
+        Page<Long> searchArticleSeqList;
+        List<ArticleResponseDTO> searchArticleList = new ArrayList<>();
 
         if (lastArticleId == null) { // 스크롤 맨처음인 경우 (첫번째 게시글부터)
-            articlesByArticleText = articleRep.findArticlesByArticleTextLastArticleIsnull(inputText, pageable);
+            searchArticleSeqList = articleRep.findArticlesByArticleTextLastArticleIsnull(userSeq, inputText, pageable);
         }
         else { // 스크롤 하단인 경우 (n번째 게시글부터)
-            articlesByArticleText = articleRep.findArticlesByArticleText(inputText, lastArticleId, pageable);
+            searchArticleSeqList = articleRep.findArticlesByArticleText(userSeq, inputText, lastArticleId, pageable);
         }
 
         // 조회된 게시글 없는 경우
-        if (articlesByArticleText == null || articlesByArticleText.getContent().isEmpty()){
+        if (searchArticleSeqList == null || searchArticleSeqList.isEmpty()){
             return null;
         }
-        // 권한에 맞게 필터링
-        searchArticleList = checkAuthSearchArticleList(userSeq, articlesByArticleText);
-        // 작성일순 정렬
-        searchArticleList.sort(new Comparator<ArticleResponseDTO>() {
-            @Override
-            public int compare(ArticleResponseDTO o1, ArticleResponseDTO o2) {
-                return o2.getWrittenDate().compareTo(o1.getWrittenDate());
-            }
-        });
 
+        // ArticleResponseDTO 가져오기
+        searchArticleSeqList.getContent().forEach(f -> searchArticleList.add(articleService.findArticleResponse(userSeq, f)));
         return searchArticleList;
-    }
-
-    /**
-     * 검색된 게시글 권한에 따라 필터링
-     * @param searchedArticleList
-     * @return
-     */
-    private List<ArticleResponseDTO> checkAuthSearchArticleList (Long userSeq, Page<Article> searchedArticleList){
-        List<ArticleResponseDTO> authArticleList = new ArrayList<>();
-
-        for (Article searchArticle: searchedArticleList.getContent()){
-            if (searchArticle.getStatus() == 1 || Objects.equals(searchArticle.getUser().getUserSeq(), userSeq)) {
-                // 전체공개인 경우 혹은 내가 쓴 글인경우
-                ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                authArticleList.add(dto);
-            }
-            else if (searchArticle.getStatus() == 2) {
-                // 맞팔공개인 경우
-                Optional<Following> followCheck = followingRepo.findByFollowingAndFollowerUserSeqVer(userSeq, searchArticle.getUser().getUserSeq());
-                if (followCheck.isPresent()){
-                    ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                    authArticleList.add(dto);
-                }
-            }
-        }
-        return authArticleList;
     }
 
 }
