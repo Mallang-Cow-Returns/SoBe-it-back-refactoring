a4b1c27a289e40f56bc56221d10757e543314910
rninji <yellowminji@naver.com>
2024-03-23 20:42:37 +0900

    [#17] REFACTOR : 게시글 검색 권한 체크 중복 로직 메소드화

--- src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
+++ src/main/java/com/finalproject/mvc/sobeit/service/SearchServiceImpl.java
@@ -53,48 +53,21 @@ public class SearchServiceImpl implements SearchService{
     @Override
     public List<ArticleResponseDTO> articlesSearch(Long userSeq, String inputText, Long lastArticleId) {
         Pageable pageable = PageRequest.of(0, 4);
-        List<ArticleResponseDTO> searchArticleList = new ArrayList<>();
+        List<ArticleResponseDTO> searchArticleList;
 
+        // 스크롤 맨처음인 경우 (첫번째 게시글부터)
         if (lastArticleId == null) {
             Page<Article> lastArticleIsnull = articleRep.findArticlesByArticleTextLastArticleIsnull(inputText, pageable);
-
-            if (lastArticleIsnull == null || lastArticleIsnull.getContent().size() == 0){
-                throw new RuntimeException("검색어가 포함된 게시글이 존재하지 않습니다.");
+            // 조회된 게시글 없는 경우
+            if (lastArticleIsnull == null || lastArticleIsnull.getContent().isEmpty()){
+                return null;
             }
-
-            for (Article searchArticle: lastArticleIsnull.getContent()){
-                if (searchArticle.getStatus() == 1 || Objects.equals(searchArticle.getUser().getUserSeq(), userSeq)) {
-                    // 전체공개인 경우
-                    ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                    searchArticleList.add(dto);
-                }
-                else if (searchArticle.getStatus() == 2) {
-                    // 맞팔공개인 경우
-                    Optional<Following> followCheck = followingRepo.findByFollowingAndFollowerUserSeqVer(userSeq, searchArticle.getUser().getUserSeq());
-                    if (followCheck.isPresent()){
-                        ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                        searchArticleList.add(dto);
-                    }
-                }
-            }
+            searchArticleList = checkAuthSearchArticleList(userSeq, lastArticleIsnull);
         }
+        // 스크롤 하단인 경우 (n번째 게시글부터)
         else {
             Page<Article> articlesByArticleText = articleRep.findArticlesByArticleText(inputText, lastArticleId, pageable);
-            for (Article searchArticle: articlesByArticleText.getContent()){
-                if (searchArticle.getStatus() == 1 || Objects.equals(searchArticle.getUser().getUserSeq(), userSeq)) {
-                    // 전체공개인 경우 혹은 내가 쓴 글인경우
-                    ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                    searchArticleList.add(dto);
-                }
-                else if (searchArticle.getStatus() == 2) {
-                    // 맞팔공개인 경우
-                    Optional<Following> followCheck = followingRepo.findByFollowingAndFollowerUserSeqVer(userSeq, searchArticle.getUser().getUserSeq());
-                    if (followCheck.isPresent()){
-                        ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
-                        searchArticleList.add(dto);
-                    }
-                }
-            }
+            searchArticleList = checkAuthSearchArticleList(userSeq, articlesByArticleText);
         }
         searchArticleList.sort(new Comparator<ArticleResponseDTO>() {
             @Override
@@ -106,6 +79,32 @@ public class SearchServiceImpl implements SearchService{
         System.out.println(searchArticleList);
 
         return searchArticleList;
+    }
+
+    /**
+     * 검색된 게시글 권한에 따라 필터링
+     * @param searchedArticleList
+     * @return
+     */
+    private List<ArticleResponseDTO> checkAuthSearchArticleList (Long userSeq, Page<Article> searchedArticleList){
+        List<ArticleResponseDTO> authArticleList = new ArrayList<>();
+
+        for (Article searchArticle: searchedArticleList.getContent()){
+            if (searchArticle.getStatus() == 1 || Objects.equals(searchArticle.getUser().getUserSeq(), userSeq)) {
+                // 전체공개인 경우 혹은 내가 쓴 글인경우
+                ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
+                authArticleList.add(dto);
+            }
+            else if (searchArticle.getStatus() == 2) {
+                // 맞팔공개인 경우
+                Optional<Following> followCheck = followingRepo.findByFollowingAndFollowerUserSeqVer(userSeq, searchArticle.getUser().getUserSeq());
+                if (followCheck.isPresent()){
+                    ArticleResponseDTO dto = articleService.findArticleResponse(userSeq, searchArticle.getArticleSeq());
+                    authArticleList.add(dto);
+                }
+            }
+        }
+        return authArticleList;
     }
 
 }
